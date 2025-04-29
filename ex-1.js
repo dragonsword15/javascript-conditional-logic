6727870ad8f0b90707c3fc2c8257f32ed09cba37
pongpetr <suerdragon8995@gmail.com>
2025-04-29 21:10:40 +0700

    Bp8-pang

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,9 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+if (lightBuldStatus === "On") {
+    console.log("Light bulb is On.")
+} else {
+    console.log("Light bulb is Off.")
+};
+
--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,10 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+lightBulbStatus === "Off" ? 
+console.log("Light bulb is On.") 
+: lightBulbStatus === "Off" ? 
+console.log("Light bult is Off.") 
+: lightBulbStatus === "Broken" ?
+console.log("Light bulb is Broken") :
+console.log("please choose the correct input (On/Off/Broken");
